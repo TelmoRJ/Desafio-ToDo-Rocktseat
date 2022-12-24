8862330c1f63989be20b56dfbefe930fda5807cb
Telmo Santos <narutoplay19@gmail.com>
2022-12-24 20:01:49 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Um desafio da rocktseat fazendo apena a logica de remover, adicionar e marca se esta completa ou nao
+
+## Tecnologia Usadas:
+### - React
+### - TypeScript
--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,26 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(!newTaskTitle) return
+    setTasks([...tasks, { id: Math.random(), isComplete: false, title: newTaskTitle }])
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+ 
+    tasks.forEach(task => {
+      if(task.id === id) {
+        task.isComplete = !task.isComplete
+        setTasks([...tasks])
+      }
+    })
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+
+    const taskDelete = tasks.filter(task => task.id !== id)
+    setTasks(taskDelete)
   }
 
   return (
